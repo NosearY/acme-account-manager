fd10e5981fc4d783ed74dd33d19c332bce08d45d
reasonyuan <reason.yx@gmail.com>
2022-08-04 23:13:30 +0800

    Update api doc

--- src/main/java/com/acmebank/accountmanager/controller/AccountController.java
+++ src/main/java/com/acmebank/accountmanager/controller/AccountController.java
@@ -5,6 +5,11 @@ import com.acmebank.accountmanager.dto.response.BalanceDto;
 import com.acmebank.accountmanager.dto.response.TransactionDto;
 import com.acmebank.accountmanager.exception.ResourceNotFoundException;
 import com.acmebank.accountmanager.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +27,13 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(summary = "Get account balance by account No.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the account with balance",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = BalanceDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Account not found",
+                    content = @Content)})
     @GetMapping(value = "/{accountNo}/balance", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BalanceDto> getAccountBalance(@PathVariable("accountNo") String accountNo) {
         return accountService.getAccountBalanceByAccountNo(accountNo)
@@ -29,6 +41,13 @@ public class AccountController {
                 .orElseThrow(ResourceNotFoundException::new);
     }
 
+    @Operation(summary = "Create a transaction for transferring money from one account to another")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction created, money transfer succeeded",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TransactionDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Transaction failure, see the error message for more detail",
+                    content = @Content)})
     @PostMapping(value = "/{accountNo}/transaction", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TransactionDto> createTransaction(@PathVariable("accountNo") String accountNo, @Valid @RequestBody CreateTransactionDto createTransactionDto) {
         return new ResponseEntity<>(accountService.createTransaction(accountNo, createTransactionDto), HttpStatus.CREATED);
